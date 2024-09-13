19a89dce5c18b9e47b98ebab136cf8b643959d5f
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-09-13 17:15:56 -0400

    cleaning and updating
    added the min digits in input be 5 and max digits would 10 also making it response with media querys with fixing button overlapping and text in input hide when width decraesed

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-const input1 = document.getElementById("input1");
+const input = document.getElementById("input1");
 const button = document.getElementById("search");
 const info =  document.getElementById("info")
 const key = '23862e68da6c3fc6f6001c4e91ae5026';
 const temp = document.getElementById("temps");
-const feeltemp = document.getElementById("feeltemp");
  const  f = (input) =>{
     return Math.floor((input -273.15) *9/5 + 32);
  }
-  console.log(f(298.706))
-const fetchData = async (input1) =>{
+  
+const fetchData = async (input) =>{
     const err ="no data";
     try{
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${input1},us&appid=${key}`);
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${input},us&appid=${key}`);
     const data = await res.json();
-    const { weather, main} = data;
-    temp.innerHTML = `The Temperature is ${f(main.temp)}f`;
-    console.log(weather[0]);
+    const { weather, main,name} = data;
+    temp.innerHTML = `The Temperature is ${f(main.temp)}f, but it feels like ${f(main.feels_like)}f`;
     console.log(data)
     }catch(err){
  console.log(err);
@@ -24,7 +22,12 @@ const fetchData = async (input1) =>{
 
 console.log(1+1);
 button.addEventListener('click',() =>{
-    fetchData(input1.value)
+    if( input.value.toString().length <  4 || input.value.toString().length >  9){
+        alert('please put in 5-10 digit number');
+    }else{
+        fetchData(input.value);
+    
+    }
 });
 
 
