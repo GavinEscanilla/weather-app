b7fd9fc63cb942685bf1c59e5de5e146a2350a49
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-09-14 22:02:00 -0400

    put degree next to the temp
    made it so if the temp is higher than 50f then background would turn red if its 50 or below  background turns blue

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const button = document.getElementById("search");
 const info =  document.getElementById("info")
 const key = '23862e68da6c3fc6f6001c4e91ae5026';
 const temp = document.getElementById("temps");
+const weatherc = document.getElementById("weather-container");
  const  f = (input) =>{
     return Math.floor((input -273.15) *9/5 + 32);
  }
@@ -13,7 +14,13 @@ const fetchData = async (input) =>{
     const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${input},us&appid=${key}`);
     const data = await res.json();
     const { weather, main,name} = data;
-    temp.innerHTML = `The Temperature is ${f(main.temp)}f, but it feels like ${f(main.feels_like)}f`;
+    temp.innerHTML = `The Temperature is ${f(main.temp)}°F, but it feels like ${f(main.feels_like)}°F`;
+    if( f(main.temp) > 50){
+        weatherc.style.background = "rgba(255, 0, 0, 0.5)";
+
+    }else{
+        weatherc.style.background = "rgba(0, 0, 255, 0.5)";
+    }
     console.log(data)
     }catch(err){
  console.log(err);
